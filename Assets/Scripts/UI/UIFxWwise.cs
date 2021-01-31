8d0e68adc8dafa8111665c4dc05762afc94e6763
Frederic <flevesque124@gmail.com>
2021-01-31 14:53:49 -0500

    UIFxWwise script created
    For the UI sound FX

---
+++ Assets/Scripts/UI/UIFxWwise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFxWwise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void UIBackSoundFX()
+    {
+        AkSoundEngine.PostEvent("UI_Back", gameObject);
+    }
+
+
+    public void UIHoverSoundFX()
+    {
+        AkSoundEngine.PostEvent("UI_Hover", gameObject);
+    }
+
+    public void UISelectSoundFX()
+    {
+        AkSoundEngine.PostEvent("UI_Select", gameObject);
+    }
+
+    public void UIStartGameSoundFX()
+    {
+        AkSoundEngine.PostEvent("UI_StartGame", gameObject);
+    }
+}
