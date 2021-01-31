49d00a7045a5fca652121b0cfec5091fee7c7eb2
pakasfand <pakasfand@gmail.com>
2021-01-31 12:18:35 -0800

    Add prefab

---
+++ Assets/LevelMusicController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMusicController : MonoBehaviour
+{
+    public TimeManager _timeManager;
+
+    private void OnEnable()
+    {
+        _timeManager.OnGameStart += OnGameStart;
+    }
+
+    private void OnDisable()
+    {
+        _timeManager.OnGameStart -= OnGameStart;
+    }
+
+    private void OnGameStart()
+    {
+        AkSoundEngine.PostEvent("Music_LevelStart", gameObject);
+    }
+}
