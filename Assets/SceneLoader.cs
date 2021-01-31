aa0e31710f9a8eea9e7b375028aea3ec1c18673c
Robb1124 <46568733+Robb1124@users.noreply.github.com>
2021-01-31 13:02:56 -0500

    Menu Navigation with controller support

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private Selectable defaultSelection;
+
+    private void Awake()
+    {
+        defaultSelection.Select();
+        defaultSelection.OnSelect(null);
+    }
+
     public void LoadGame()
     {
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
