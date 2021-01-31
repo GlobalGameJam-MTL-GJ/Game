4859eb93d89d3e8650f3cc8db30d98f80f7fca53
Frederic <flevesque124@gmail.com>
2021-01-30 20:41:17 -0500

    Level Selection menu added
    Level selection added

---
+++ Assets/Scripts/UI/UIMenuNaviguation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMenuNaviguation : MonoBehaviour
+{
+    private InputSystemUIInputModule inputSystem;
+    private PlayerInputActions inputActions;
+    private bool m_IsLevel2Locked = true;
+    public bool IsLevel2Locked { get; set; }
+
+    private bool m_IsLevel3Locked = true;
+    public bool IsLevel3Locked { get; set; }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level_02");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level_03");
+    }
+
+    public void GoBackButton()
+    {
+        SceneManager.LoadScene("TOBEREPLACEDWITHMENUSCENE");
+    }
+}
